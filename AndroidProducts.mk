c1001a208a79e7ca3e71a91105dd7f381e569db8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-03-17 17:55:49 +0100

    TECNO-KG8: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO KG8 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-KG8.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-KG8-user \
+    omni_TECNO-KG8-userdebug \
+    omni_TECNO-KG8-eng
---
+++ omni_TECNO-KG8.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from TECNO-KG8 device
+$(call inherit-product, device/tecno/TECNO-KG8/device.mk)
+
+PRODUCT_DEVICE := TECNO-KG8
+PRODUCT_NAME := omni_TECNO-KG8
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO KG8
+PRODUCT_MANUFACTURER := tecno
+
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_kg8_h698-user 11 RP1A.200720.011 262975 release-keys"
+
+BUILD_FINGERPRINT := TECNO/KG8-GL/TECNO-KG8:11/RP1A.200720.011/230303V586:user/release-keys
